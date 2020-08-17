d415f916a95e610727240980f657a7ff1e3ede4a
dom3 <52584844+dom3@users.noreply.github.com>
2020-08-17 15:36:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const {Client} = require('discord.js');
+const client = new Client();
+
+var cookies = 0
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+    client.user.setActivity('' + cookies + ' cookies.', { type: 'WATCHING' });
+});
+
+client.on('message', message => {
+    if (message.member.user.bot) return;
+
+    if (message.content == "cookie" || message.content == "Cookie") {
+        var newCookies = cookies + 1;
+
+        cookies = newCookies
+        client.user.setActivity('' + newCookies + ' cookies.', { type: 'WATCHING' });
+        message.channel.send('Cookies: ' + newCookies)
+    } else {
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
+            message.delete();
+        } else {
+            return;
+        }
+    }
+
+})
+
+client.login(process.env.token);
